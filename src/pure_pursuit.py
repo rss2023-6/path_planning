31a5ae22c0cd1c66f285235c026916359d38c475
Penny <pennyb@mit.edu>
2023-04-19 05:29:11 -0400

    fixed circle finding and adding frame transfortations

--- src/pure_pursuit.py
+++ src/pure_pursuit.py
@@ -6,6 +6,7 @@ import time
 import utils
 import tf
 
+from tf.transformations import euler_from_quaternion, quaternion_from_euler
 from geometry_msgs.msg import PoseArray, PoseStamped
 from visualization_msgs.msg import Marker
 from ackermann_msgs.msg import AckermannDriveStamped
@@ -16,49 +17,54 @@ class PurePursuit(object):
     """
     def __init__(self):
         self.odom_topic       = rospy.get_param("~odom_topic","/pf/pose/odom")
-        self.lookahead        = 0.5 #filled in for testing purposes, please update, larger is more smooth and smaller is more oscillations
+        self.lookahead        = 1.5 #filled in for testing purposes, please update, larger is more smooth and smaller is more oscillations
         self.speed            = 1 #filled in for testing purposes, please update
-        self.wheelbase_length = 0.24 #flilled in for testing purposes, please update
-
+        self.wheelbase_length = 0.32 #flilled in for testing purposes, please update
+        self.trajectory  = utils.LineTrajectory("/followed_trajectory")
         self.traj_sub = rospy.Subscriber("/trajectory/current", PoseArray, self.trajectory_callback, queue_size=1)
         self.drive_pub = rospy.Publisher("/drive", AckermannDriveStamped, queue_size=1)
         self.odom_sub = rospy.Subscriber(self.odom_topic, Odometry, self.odom_callback, queue_size=1)
-        self.previous_odom_message = None
         self.current_location = np.array([0,0])
         self.x = self.current_location[0]
         self.y = self.current_location[1]
         self.theta = 0
         
-
-    def odom_callback(self, msg):
-        if(self.previous_odom_message != None):
-            curposition = msg.pose.pose.position
-            self.current_location = np.array([curposition.x,curposition.y])
-            self.x = curposition.x
-            self.y = curposition.y
-            self.trajectory  = utils.LineTrajectory("/followed_trajectort")
-            self.n = len(self.trajectory.points)
-            rospy.logerr(self.trajectory.points)
-        self.previous_odom_message = msg
-
     def trajectory_callback(self, msg):
         ''' Clears the currently followed trajectory, and loads the new one from the message
         '''
         #print "Receiving new trajectory:", len(msg.poses), "points"
-        
         self.trajectory.clear()
         self.trajectory.fromPoseArray(msg)
         self.trajectory.publish_viz(duration=0.0)
-        coordarr = self.get_coordinate_array(msg.poses)
+
+    def odom_callback(self, msg):
+        curposition = msg.pose.pose.position
+        curorientation = msg.pose.pose.orientation
+        self.current_location = np.array([curposition.x,curposition.y])
+        self.x = curposition.x
+        self.y = curposition.y
+        poses = self.trajectory.points
+        if(poses == []):
+            return
+        
+        self.n = len(poses)
+        coordarr = self.get_coordinate_array2(poses)
         index = self.find_closest_segment_index(coordarr,self.current_location)
         goalpos = self.find_circle_intersection(index)
-        #rospy.logerr(self.current_location)
-        if(goalpos != (0,0)):
+
+        if(goalpos != (0, 0)):
+            goalpos = self.transformtocarframe(goalpos, curorientation, curposition)
+            goalpos = (goalpos[0], goalpos[1])
             self.drive_command(goalpos[0],goalpos[1])
+
 
     def get_coordinate_array(self, poses):
         #Return an array of line segements where each segement represented as a 2x2 array of start and end coordinates
         return np.array([[pose.position.x, pose.position.y] for pose in poses])
+    
+    def get_coordinate_array2(self, poses):
+        #Return an array of line segements where each segement represented as a 2x2 array of start and end coordinates
+        return np.array([[pose[0], pose[1]] for pose in poses])
     
     def get_minimum_distance(self, v, w, p):
         #Return minimum distance between line segment vw and point p
@@ -79,7 +85,7 @@ class PurePursuit(object):
         return np.argmin(closest_points)
 
     def find_circle_intersection(self, index):
-        radius = self.lookahead
+        radius = self.speed * 0.6
         rx = self.x
         ry = self.y
         Q = np.array([rx,ry])
@@ -87,84 +93,83 @@ class PurePursuit(object):
         #assume the index is the index of the line segment that the closest point lines on
 
         for i in range(index, self.n - 1):
-            x0 = self.trajectory[i][0]
-            y0 = self.trajectory[i][1]
-            x1 = self.trajectory[i + 1][0]
-            y1 = self.trajectory[i + 1][1]
-            rospy.logerr("seg: ({},{})->({},{})".format(x0,y0,x1,y1))
-            #ax + by = c
-            a = y1 - y0
-            b = x1 - x0
-            c = x0*y1 - x1*y0
-            #@Fritz's that calcualte the intersection of circle and line, given ax + by = c and circle at rx, ry with radius r
+            x0 = self.trajectory.points[i][0]
+            y0 = self.trajectory.points[i][1]
+            x1 = self.trajectory.points[i + 1][0]
+            y1 = self.trajectory.points[i + 1][1]
 
-            # convert line to slope intercept form, and then vectorize it
-            mb = np.array([-a/b,c/b])
             p1 = np.array([x0,y0])
             p2 = np.array([x1,y1])
-            v = (p2-p1)/np.linalg.norm(p2-p1) #Unit Norm
-            # q_ = variables for quadratic equation for finding points on circ
-            qa = v.dot(v)
-            qb = 2*v.dot(p1-Q)
-            qc = p1.dot(p1) + Q.dot(Q) - 2*p1.dot(Q) - radius**2
+            v = (p2-p1)
 
-            disc = b**2-4*a*c # If negative, line doesn't intersect circle
-            rospy.logerr("disc: {}".format(disc))
+            qa = np.dot(v, v)
+            qb = 2.0 * np.dot(v, p1 - Q)
+            qc = np.dot(p1, p1) + np.dot(Q, Q) - 2.0 * np.dot(p1, Q) - radius**2
+            print("seg: ({},{})->({},{})".format(x0,y0,x1,y1))
+
+            disc = qb**2.0-4.0*qa*qc # If negative, line doesn't intersect circle
+            print("disc: {}".format(disc))
             if(disc < 0):
                 continue
-            t = np.array([(-b+np.sqrt(disc))/(2*a),(-b-np.sqrt(disc))/(2*a)])
-            t = np.abs(t) # take abs value of t values to ensure direction along line segment is correct
+            t = np.array([(-1.0 * qb+np.sqrt(disc))/(2.0*qa),(-1.0 * qb-np.sqrt(disc))/(2.0*qa)])
+            print(t)
+            # take abs value of t values to ensure direction along line segment is correct
             # if either of the t's are outside of (0,1), line segment doesn't touch circle
-            solutionindex = [0]
-            rospy.logerr(t)
-            if(t[0] > 1 or t[0] <= 0):
-                solutionindex = [1]
-                if(t[1] > 1 or t[1] < 0):
-                    continue
-            else:
-                if(t[1] > 1 or t[1] < 0):
-                    solutionindex = [0]
-                else:
-                    solutionindex = [0, 1]
-
-            intersect = np.array([[0, 0], [0, 0]])
-            for i in solutionindex:
-                intersectpoints = np.array([p1+t[i]*v,p1+t[i]*v]) # Np array of both intersect points
-                self.intersectx = intersectpoints[i][0] # arbitrarily picks first intersect point, may need to be changed
-                self.intersecty = intersectpoints[i][1]
-
-                if(self.intersectx >= x0 and self.intersectx <= x1):
-                    return (self.intersectx, self.intersecty)
-                    found = True
-                    break
-        if(found):
-            return (self.intersectx, self.intersecty)
-        else:
-            rospy.logerr("no intersection found")
-            return (0,0)
+            solutionpoints = []
+            if(t[0] < 1 and t[0] > 0):
+              p = p1 + t[0] * v
+              solutionpoints.append((p[0], p[1]))
+              found = True
+            if(t[1] < 1 and t[1] > 0):
+              p = p1 + t[1] * v
+              solutionpoints.append((p[0], p[1]))
+              found = True
+            if(found):
+                return solutionpoints[-1]
+        return (0, 0)
 
     def get_curvature(self, goalx, goaly):
         return 2 * goalx / self.lookahead**2
     
-    def find_lookhead(self):
-        pass
+    def get_rotation_matrix(self, theta):
+        return np.array([[np.cos(theta), -1 * np.sin(theta)], [np.sin(theta), np.cos(theta)], [0, 0, 1, 0], [0, 0, 0, 1]])
+    
+    def transformtocarframe(self, goalpos, orientation, position):
+        roll, pitch, theta = euler_from_quaternion([orientation.x, orientation.x, orientation.z, orientation.w])
+        rotation_matrix = self.get_rotation_matrix(theta)
+        trans_matrix = np.array([[1, 0, 0, position.x], [0, 1, 0, position.y], [0, 0, 1, position.z], [0, 0, 0, 1]])
+        
+        combined_trans = np.linalg.inv( np.matmul(trans_matrix, rotation_matrix))
+        rospy.loginfo(combined_trans)
 
+        goal = np.array([[goalpos[0]], [goalpos[1]], [0], [1]])
+        final_goal = np.matmul(combined_trans, goal)
+        rospy.logerr("FINAL GOAL")
+        rospy.logerr(final_goal)
+        return final_goal
+    
     def drive_command(self, goalx, goaly):
-        if(goaly == 0):
+        rospy.logerr("x: {}, y: {}".format(self.x, self.y))
+        rospy.logerr("goalx: {}, goaly: {}".format(goalx, goaly))
+        rospy.logerr("a")
+        diffx = goalx - self.x
+        diffy = goaly - self.y
+
+        if(diffy == 0):
             eta = 0
         else:
-            eta = np.arctan2(goaly, goalx)
+            eta = np.arctan2(goalx, goaly)
         # R = self.lookahead / (2 * np.sin(eta))
         AckermannDrive = AckermannDriveStamped()
-        AckermannDrive.header.stamp = rospy.time.now()
+        AckermannDrive.header.stamp = rospy.Time.now()
         AckermannDrive.header.frame_id = "base_link"
         AckermannDrive.drive.speed = self.speed
-        AckermannDrive.drive.steering_angle = np.arctan(2 * self.wheelbase_length * np.sin(eta) / self.lookahead)
-        
+        AckermannDrive.drive.steering_angle = np.arctan2(2 * self.wheelbase_length * np.sin(eta) / self.lookahead)
+
         #generalized sttering law by having a point ahead lecture slides
         # lfw = 0.05 #randomly defined based on lecture slides
         # AckermannDrive.drive.steering_angle = -1 * np.arctan(self.wheelbase_length * np.sin(eta) / (self.lookahead / 2  + lfw/np.cos(eta)))
-        self.drive_pub.pub(AckermannDrive)
+        self.drive_pub.publish(AckermannDrive)
 
 
 if __name__=="__main__":
