a52e43a204fcc48017e5efdec2dd29fac5509002
kristinezheng <74512693+kristinezheng@users.noreply.github.com>
2023-04-24 20:10:32 -0400

    a star working in stata

--- src/path_planning.py
+++ src/path_planning.py
@@ -10,6 +10,9 @@ import time, os
 from utils import LineTrajectory
 from tf.transformations import euler_from_quaternion
 from Queue import PriorityQueue, Queue
+from scipy.spatial.transform import Rotation as R
+
+from scipy.misc import imread, imsave #scipy 1.2.0
 
 class PathPlan(object):
     """ Listens for goal pose published by RViz and uses it to plan a path from
@@ -35,39 +38,32 @@ class PathPlan(object):
         self.map_cols = None
         self.map_origin = None
         self.current_pos = None
-        self.car_length = 0.35
+        self.car_length = 0.5 #0.35
         self.car_box_length = None
         self.search_step_size = 5
         self.seen = set()
+
+        self.init_time = None #rospy.get_rostime()
+        self.final_time = None
         rospy.loginfo("planning initialized")
 
-    # def transformation_matrix(self, th):
-    #     return np.array([[np.cos(th), -np.sin(th), 0],
-    #                         [np.sin(th), np.cos(th), 0],
-    #                         [0, 0, 1]])
-    
-    # def px_2_m(self, map_resolution, px):
-    #     return px*float(map_resolution)*self.lidar_scale_to_map_scale
-
-    # def m_2_px(self, m):
-    #     return m/(float(self.map_resolution)*self.lidar_scale_to_map_scale)
 
     def map_cb(self, msg): ######
         self.map_resolution = msg.info.resolution
-        rospy.loginfo("map_resolution")
-        rospy.loginfo(self.map_resolution)
+        # rospy.loginfo("map_resolution")
+        # rospy.loginfo(self.map_resolution)
         q = msg.info.origin.orientation
         roll, pitch, th = euler_from_quaternion([q.x, q.y, q.z, q.w])
-        rospy.loginfo("euler_from_quaternion")
-        rospy.loginfo(euler_from_quaternion([q.x, q.y, q.z, q.w]))
-        self.map_orientation = th 
+        # rospy.loginfo("euler_from_quaternion")
+        # rospy.loginfo(euler_from_quaternion([q.x, q.y, q.z, q.w]))
+        self.map_orientation = q#th 
         self.map_origin = msg.info.origin.position
-        self.WorldToMapTransform = np.array([[np.cos(-self.map_orientation), -np.sin(-self.map_orientation), -self.map_origin.x],
-                                      [np.sin(-self.map_orientation), np.cos(-self.map_orientation), -self.map_origin.y],
-                                      [0, 0, 1]])
+        # self.WorldToMapTransform = np.array([[np.cos(-self.map_orientation), -np.sin(-self.map_orientation), -self.map_origin.x],
+        #                               [np.sin(-self.map_orientation), np.cos(-self.map_orientation), -self.map_origin.y],
+        #                               [0, 0, 1]])
 
-        rospy.loginfo("self.map_origin")
-        rospy.loginfo(self.map_origin)
+        # rospy.loginfo("self.map_origin")
+        # rospy.loginfo(self.map_origin)
 
         
         #discretize map 
@@ -75,26 +71,28 @@ class PathPlan(object):
         self.map_cols = msg.info.width
         self.car_box_length = int(self.car_length/(self.map_resolution * 2))
         map = np.array(msg.data).reshape((self.map_rows, self.map_cols)) #rows, cols
-        self.map_grid = np.where(np.logical_and(0 <= map, map < 0.5), map, 1) #set to 1 if negative or greater than 0.5 aka occupied or unknown
+        self.map_grid = np.where(np.logical_and(0 <= map, map < 0.5), 0, 1) #set to 1 if negative or greater than 0.5 aka occupied or unknown
+        # imsave("/home/racecar/racecar_ws/src/path_planning/maps/occupancy2.png", (np.true_divide(self.map_grid, 100.0) * 255.0).T)
        
 
     def odom_cb(self, msg): ######
         #q = msg.pose.pose.orientation #not sure if we need this 
         #roll, pitch, th = euler_from_quaternion([q.x, q.y, q.z, q.w])
-        self.current_pos = np.array([msg.pose.pose.position.x, msg.pose.pose.position.y])
+        self.current_pos = np.array([msg.pose.pose.position.x, msg.pose.pose.position.y]) #np.array([-18.0, -9.0])#
         self.initialized = True 
 
     def goal_cb(self, msg): ######
         if self.initialized:
             start_pt = self.current_pos
-            goal_pt = msg.pose.position.x, msg.pose.position.y
+            goal_pt = msg.pose.position.x, msg.pose.position.y #(6,18) #
+            self.init_time = rospy.get_rostime()
 
             # rospy.loginfo("start and end in world")
             # rospy.loginfo(start_pt)
             # rospy.loginfo(goal_pt)
 
-            start_px = self.world_to_map_frame(start_pt[0],start_pt[1])#change to map frame 
-            end_px = self.world_to_map_frame(goal_pt[0],goal_pt[1])  #change to map frame 
+            start_px = (self.world_to_pixel_frame(start_pt[0],start_pt[1]))#change to map frame 
+            end_px = (self.world_to_pixel_frame(goal_pt[0],goal_pt[1]))  #change to map frame 
 
             # rospy.loginfo("start and end in map")
             # rospy.loginfo(start_px)
@@ -114,16 +112,20 @@ class PathPlan(object):
         path = self.a_star(start_point, end_point, map)
 
         if path:
+            self.final_time = rospy.get_rostime()
+            rospy.loginfo("time duration")
+            diff = self.final_time - self.init_time
+            rospy.loginfo(diff.to_sec())
             #change map frame back to world frame 
-            # rospy.loginfo("path")
-            # rospy.loginfo(path)
+            rospy.loginfo("path")
+            rospy.loginfo(self.total_distance(path))
             
             world_path = []
             for tup in path:
-                world_path.append(self.map_to_world_frame(tup[0],tup[1]))
-            # rospy.loginfo("world_path")
-            # rospy.loginfo(world_path)
-
+                world_path.append(self.pixel_to_world_frame(tup[0],tup[1]))
+            rospy.loginfo("world_path")
+            rospy.loginfo(self.total_distance(world_path))
+            
             self.trajectory.points  = world_path
 
         # publish trajectory
@@ -132,6 +134,11 @@ class PathPlan(object):
         # visualize trajectory Markers
         self.trajectory.publish_viz(60)
 
+    def total_distance(self, path):
+        sum = 0
+        for i in range(len(path)-1):
+            sum += self.map_cost(path[i], path[i+1])
+        return sum
 
     def map_cost(self, current, next):
         '''calculates distance'''
@@ -142,17 +149,42 @@ class PathPlan(object):
         dist = np.sqrt((goal[0]-next[0])**2 + (goal[1]-next[1])**2)
         return dist  
     
-    def world_to_map_frame(self, nx, ny):
-        norm_point = np.array([nx, ny, 1])
-        px_point = np.matmul(self.WorldToMapTransform, norm_point)/self.map_resolution
-        return (px_point[0], px_point[1])
+    # def world_to_map_frame(self, nx, ny):
+    #     norm_point = np.array([nx, ny, 1])
+    #     px_point = np.matmul(self.WorldToMapTransform, norm_point)/self.map_resolution
+    #     return (px_point[0].astype("uint8"), px_point[1].astype("uint8"))
     
-    def map_to_world_frame(self, nx, ny):
-        norm_point = np.array([nx * self.map_resolution, ny * self.map_resolution, 1])
-        MapToWorldTransform = np.linalg.inv(self.WorldToMapTransform)
-        world_pt = np.matmul(MapToWorldTransform, norm_point)
+    # def map_to_world_frame(self, nx, ny):
+    #     norm_point = np.array([nx * self.map_resolution, ny * self.map_resolution, 1])
+    #     MapToWorldTransform = np.linalg.inv(self.WorldToMapTransform)
+    #     world_pt = np.matmul(MapToWorldTransform, norm_point)
 
-        return (world_pt[0], world_pt[1])
+    #     return (world_pt[0], world_pt[1])
+
+    def world_to_pixel_frame(self, nx, ny):
+        real_pos = np.array([nx, ny, 1]).T
+        q = [self.map_orientation.x, self.map_orientation.y, self.map_orientation.z, self.map_orientation.w]
+        rot = R.from_quat(q)
+        rot_pix_to_world = rot.as_dcm()
+        rot_pix_to_world[0,2] = self.map_origin.x
+        rot_pix_to_world[1,2] = self.map_origin.y
+
+        rot_world_to_pixel =  np.linalg.inv(rot_pix_to_world)
+
+        px_point = np.matmul(rot_world_to_pixel, real_pos)/self.map_resolution
+        return (px_point[0].astype(int), px_point[1].astype(int))
+    
+    def pixel_to_world_frame(self, nx, ny):
+        pix_pos = np.array([nx * self.map_resolution, ny * self.map_resolution, 1]).T
+        q = [self.map_orientation.x, self.map_orientation.y, self.map_orientation.z, self.map_orientation.w]
+        rot = R.from_quat(q)
+        rot_pix_to_world = rot.as_dcm()
+        rot_pix_to_world[0,2] = self.map_origin.x
+        rot_pix_to_world[1,2] = self.map_origin.y
+
+        real_point = np.matmul(rot_pix_to_world, pix_pos)
+        return (real_point[0], real_point[1])
+
     
     def get_space_around_car(self, map_coords):
         map_x = int(map_coords[0])
@@ -161,6 +193,7 @@ class PathPlan(object):
         car_slice = self.map_grid[map_y - self.car_box_length : map_y + self.car_box_length, map_x - self.car_box_length : map_x + self.car_box_length]
         
         total_occupancy = np.sum(car_slice, axis=None)
+        # rospy.loginfo(total_occupancy)
         if total_occupancy != 0:
             # rospy.loginfo(map_x)
             # rospy.loginfo(map_y)
@@ -170,20 +203,24 @@ class PathPlan(object):
 
     def neighbors(self, pt):
         results = set()
-        xv, yv = np.meshgrid([-self.search_step_size, 0, self.search_step_size], [-self.search_step_size, 0, self.search_step_size], indexing='ij')
-        for i in range(3):
-            for j in range(3):
-                nx, ny = pt[0] + xv[i,j], pt[1] + yv[i,j] #indexing confusion
-                map_coords = (nx, ny)
-                if (nx, ny) not in self.seen:
-                    self.seen.add((nx, ny))
-                    if self.get_space_around_car(map_coords):
-                        results.add((nx, ny))
+        #xv, yv = np.meshgrid([-self.search_step_size, 0, self.search_step_size], [-self.search_step_size, 0, self.search_step_size], indexing='ij')
+        for i in [-self.search_step_size, 0, self.search_step_size]: #range(3):
+            for j in [-self.search_step_size, 0, self.search_step_size]:#in range(3):
+                if (i != 0) and (j !=0):
+                    #nx, ny = pt[0] + xv[i,j], pt[1] + yv[i,j] #indexing confusion
+                    nx, ny = pt[0] + i, pt[1] + j
+                    map_coords = (nx, ny)
+                    if (nx, ny) not in self.seen:
+                        self.seen.add((nx, ny))
+                        if self.get_space_around_car(map_coords):
+                            results.add((nx, ny))
+                            # rospy.loginfo("results added")
+                            # rospy.loginfo((nx, ny))
         return results
 
     def a_star(self, init, goal, map):
         frontier = PriorityQueue() 
-        frontier.put((self.dist_heuristic(goal, init), init))
+        frontier.put((0, init))
 
         came_from = dict()
         cost_so_far = dict()
